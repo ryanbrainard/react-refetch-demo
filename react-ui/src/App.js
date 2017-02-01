5457ff47e8b4c76a302990610c1d26dfb2dd7fc7
Ryan Brainard <brainard@heroku.com>
2017-02-01 16:40:31 +0900

    Swap in react-refetch

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,38 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import 'whatwg-fetch';
+import { connect } from 'react-refetch';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      message: null,
-      fetching: true
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
-  }
-
   render() {
     return (
       <div className="App">
@@ -47,13 +19,25 @@ class App extends Component {
           </a><br/>
         </p>
         <p className="App-intro">
-          {this.state.fetching
-            ? 'Fetching message from API'
-            : this.state.message}
+          { this.renderMessage() }
         </p>
       </div>
     );
   }
+
+  renderMessage() {
+    const { apiFetch } = this.props
+
+    if (apiFetch.pending) {
+      return 'Fetching message from API'
+    } else if (apiFetch.rejected) {
+      return `API call failed: ${apiFetch.reason.message}`
+    } else if (apiFetch.fulfilled) {
+      return apiFetch.value.message
+    }
+  }
 }
 
-export default App;
+export default connect((props, context) => ({
+  apiFetch: '/api'
+}))(App);
